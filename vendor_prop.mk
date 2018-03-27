485b3e158f91db8dcf6073d906d902eee9148609
Connor Kirk <connkirk@codeaurora.org>
2018-03-27 14:53:45 -0700

    whyred: Rename persist.radio.atfwd.start property
    Rename to persist.vendor.radio.atfwd.start.
    This property is now qualified as a platform property and is not
    whitelisted. Therefore it must be renamed to continue to be
    used as a trigger.
    
    Change-Id: Ifec0abd5ad04cd6445c856cba540d9e85ce54f0c
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -311,7 +311,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 #Bringup properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.atfwd.start=true
+    persist.vendor.radio.atfwd.start=true
 
 #set maximum supported adapter voltage
 PRODUCT_PROPERTY_OVERRIDES += \
