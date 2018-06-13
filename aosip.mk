b6b914d5a6df22ea21750fd410599341778a34aa
Akhil Narang <akhilnarang.1999@gmail.com>
2018-06-13 19:13:02 +0530

    whyred: AOSiP bringup
      * Setup makefiles and dependencies for AOSiP
      * Delete overlay-lineage, add the required ones in overlay-aosip
      * Remove unneeded lineage-sdk dependency on XiaomiDoze
      * Update defconfig name
      * Update extract-utils for AOSiP
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -22,14 +22,14 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_whyred
+PRODUCT_NAME := aosip_whyred
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosip
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
