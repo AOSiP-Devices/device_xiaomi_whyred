LineageOS/android_device_xiaomi_whyred

commit a2fd40ee4ae4dda4af467b0e6e5b3efc1b41142c

whyred: Use MIUI poweroff charging animation
 * It's cool, and more importantly... it actually works!
   Oh look, it even displays battery level. Wow. :-P

Change-Id: I6351e2eac6df89aa1b6b98e4e6b2815fa5c37794
Signed-off-by: Subhaam <derksoul98@gmail.com>
Signed-off-by: Shahan-mik3 <mik3dev93@gmail.com>

rootdir/Android.mk
@@ -184,4 +184,13 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.goodix.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE          := chargeonlymode
+LOCAL_MODULE_OWNER    := xiaomi
+LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES       := sbin/chargeonlymode
+LOCAL_MODULE_TAGS     := optional
+LOCAL_MODULE_CLASS    := EXECUTABLES
 include $(BUILD_PREBUILT)

device.mk
@@ -299,6 +299,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Healthd
+PRODUCT_PACKAGES += \
+    chargeonlymode
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \

rootdir/root/init.qcom.rc
@@ -1038,10 +1038,10 @@ service profiler_daemon /system/bin/profiler_daemon
     group root
     disabled
 
-service charger /charger
+service charger /sbin/chargeonlymode
     class charger
     group system graphics
-    seclabel u:r:healthd:s0
+    seclabel u:r:charger:s0
 
 service ssr_diag /system/vendor/bin/ssr_diag
     class late_start

rootdir/root/init.target.rc
@@ -424,6 +424,7 @@ on charger
     start hvdcp_opti
     setprop sys.usb.configfs 1
     start power_off_alarm
+    start thermal-engine
 
 #
 #service nqs /system/bin/nqs

AOSiP-Devices/device_xiaomi_whyred

commit 76d785e936a749a0e121a670ef24d2d567d1cbb1

rootdir: Android.mk: fix copying of goodix script
This broke in commit a2fd40ee4ae4dda4af467b0e6e5b3efc1b41142c

[    7.668496] init: cannot find '/vendor/bin/init.goodix.sh', disabling 'goodix_script': No such file or directory

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

rootdir/Android.mk
@@ -184,8 +184,9 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.goodix.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(CLEAR_VARS)
+include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE          := chargeonlymode
 LOCAL_MODULE_OWNER    := xiaomi
 LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)