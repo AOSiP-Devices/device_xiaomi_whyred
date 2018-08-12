54ef7318b1fff6608521d8f45dd704e9711001f8
Shekhawat2 <anandsingh215@yahoo.com>
2018-08-12 19:09:52 +0000

    whyred: Set vendor props via PRODUCT_PROPERTY_OVERRIDES
    * Support for vendor.prop was abandoned
    * also move all props to vendor
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
