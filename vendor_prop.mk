47752276840ae0e154500890b65628b50534554c
Akhil Narang <akhilnarang.1999@gmail.com>
2018-09-30 00:43:39 +0530

    whyred: vendor_prop: remove duplicate dalvik.vm.heapsize
    It was introduced in commit 54ef7318b1fff6608521d8f45dd704e9711001f8
    
    This caused the value to be set incorrectly to 36 instead of 512, which in turn
    caused apps to go OOM and FC often
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -11,7 +11,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapmaxfree=8m \
     dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapsize=36m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heaptargetutilization=0.75 \
