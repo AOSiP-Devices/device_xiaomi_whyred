cc3d13fba4b9e64550dc2493eb11f4e5b64073bb
Akhil Narang <akhilnarang.1999@gmail.com>
2018-10-30 12:00:52 +0530

    whyred: go back to 9.5.11 fingerprint
      * Hopefully safetynet passes with this
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -37,8 +37,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="whyred" \
     PRODUCT_NAME="whyred" \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 8.10.25 release-keys"
+    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
 
-BUILD_FINGERPRINT="xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/8.10.25:user/release-keys"
+BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
 
 TARGET_VENDOR := Xiaomi
