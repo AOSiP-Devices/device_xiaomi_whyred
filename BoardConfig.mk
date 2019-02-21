9fb7ef2a64fbc737c47213e7c769adb4d7a9437e
Akhil Narang <akhilnarang.1999@gmail.com>
2019-02-21 14:57:45 +0530

    whyred: nobody here is building TWRP
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,11 +237,6 @@ BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
 
-# TWRP Support
-ifeq ($(WITH_TWRP),true)
--include $(DEVICE_PATH)/twrp.mk
-endif
-
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
 
