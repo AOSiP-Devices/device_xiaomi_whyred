8a4f45313913a7c9a6d057e1c6da71aa822f46da
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-03 12:14:21 +0800

    whyred: proprietary: Update proprietary files list
    [akhil: nuke mlipay stuff]
    
    Change-Id: Ia7c25d320403d8982f6f135de638cd4370c871ff
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -61,12 +61,4 @@ GOODIX="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/libgf
 
 sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" $GOODIX
 
-BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-patchelf --set-soname libicuuc-v27.so $BLOB_ROOT/vendor/lib/libicuuc-v27.so
-patchelf --set-soname libminikin-v27.so $BLOB_ROOT/vendor/lib/libminikin-v27.so
-
-patchelf --replace-needed android.frameworks.sensorservice@1.0.so android.frameworks.sensorservice@1.0-v27.so $BLOB_ROOT/vendor/lib/libvidhance_gyro.so
-patchelf --replace-needed libminikin.so libminikin-v27.so $BLOB_ROOT/vendor/lib/libMiWatermark.so
-patchelf --replace-needed libicuuc.so libicuuc-v27.so $BLOB_ROOT/vendor/lib/libMiWatermark.so
-
 "$MY_DIR"/setup-makefiles.sh
