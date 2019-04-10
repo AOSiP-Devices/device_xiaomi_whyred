755fbf88f7ab4a62e2b7bdb5d1c67e0172e7df9b
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-10 14:47:17 +0800

    whyred: device: Build services-ext
    Change-Id: I397d9bc624834544a88f4cd7b9dfe9b5e544082b
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -144,7 +144,8 @@ PRODUCT_PACKAGES += \
 # CNE
 PRODUCT_PACKAGES += \
     cneapiclient \
-    com.quicinc.cne
+    com.quicinc.cne \
+    services-ext
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
